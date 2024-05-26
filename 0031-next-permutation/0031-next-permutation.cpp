0624aaa02effa2813b4440fd7f0e57e6a60a29f6
Anay Sinhal <91818144+anayy09@users.noreply.github.com>
2024-05-26 22:50:24 +0530

    Time: 0 ms (100.00%), Space: 14.7 MB (14.10%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(std::vector<int>& nums) {
-        std::next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int k = -1;
+        
+        for (int i = n - 2; i >= 0; --i) {
+            if (nums[i] < nums[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+        
+        if (k == -1) {
+            std::reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        int l = -1;
+        for (int i = n - 1; i > k; --i) {
+            if (nums[i] > nums[k]) {
+                l = i;
+                break;
+            }
+        }
+        
+        std::swap(nums[k], nums[l]);
+        
+        std::reverse(nums.begin() + k + 1, nums.end());
     }
 };
